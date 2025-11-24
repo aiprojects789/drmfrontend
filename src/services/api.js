bfc73dc51b9e432925a17ab56c1ada78271f2a34
Rohit Dhanjee <145933724+RohitDhanjee@users.noreply.github.com>
2025-11-24 23:41:11 +0500

    Refactor API base URL assignment

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_BASE_URL =
-  import.meta.env.VITE_BASE_URL_BACKEND || "http://localhost:8000/api/v1";
+  import.meta.env.VITE_BASE_URL_BACKEND;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
